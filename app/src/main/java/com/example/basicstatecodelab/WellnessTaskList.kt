94b149d8eadabaecd7d38a417a5ddc694c5cd235
tayuta914 <tayuta914@gmail.com>
2025-02-08 16:48:43 +0900

    ViewModel追加

--- app/src/main/java/com/example/basicstatecodelab/WellnessTaskList.kt
+++ app/src/main/java/com/example/basicstatecodelab/WellnessTaskList.kt
@@ -3,29 +3,27 @@ package com.example.basicstatecodelab
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 
 @Composable
 fun WellnessTasksList(
     list: List<WellnessTask>,
+    onCheckedTask: (WellnessTask, Boolean) -> Unit,
     onCloseTask: (WellnessTask) -> Unit,
     modifier: Modifier = Modifier,
 ) {
     LazyColumn(
         modifier = modifier
     ) {
-        items(list) { task ->
-            var checkedState by rememberSaveable { mutableStateOf(false) }
-            WellnessTaskItem(
-                taskName = task.label,
-                checked = checkedState,
-                onCheckedChange = { newValue -> checkedState = newValue },
-                onClose = {onCloseTask(task)}
-            )
+        /**
+         * task.id を使って「このアイテムはどのタスクか」を正しく判断し、
+         * 並び替えや削除があっても適切にUIを保つことができる。
+         **/
+        items(items = list, key = { task -> task.id }) { task ->
+            WellnessTaskItem(taskName = task.label,
+                checked = task.checked,
+                onCheckedChange = { checked -> onCheckedTask(task, checked)},
+                onClose = { onCloseTask(task) })
         }
     }
 }
